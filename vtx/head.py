32147648828fe51a934a420d513410011c2b6809
Luciferian Ink <LuciferianInk@protonmail.com>
2023-01-27 13:11:28 -0600

    o

--- vtx/head.py
+++ vtx/head.py
@@ -149,7 +149,7 @@ def gen(bias=None, ctx=None):
             return_as_list=True,
             num_beams=3,
             repetition_penalty=2.0,
-            length_penalty=-0.9,
+            length_penalty=1.9,
             no_repeat_ngram_size=2,
             early_stopping=True,
             renormalize_logits=True,

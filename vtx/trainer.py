506d4e064856311ef278a9cf1cbcfd63edb928e7
Luciferian Ink <LuciferianInk@protonmail.com>
2023-05-29 12:59:35 -0500

    o

--- vtx/trainer.py
+++ vtx/trainer.py
@@ -325,8 +325,6 @@ if __name__ == "__main__":
             setattr(ai.model.config, "resid_dropout", stage["dropout"])
             setattr(ai.model.config, "summary_first_dropout", stage["dropout"])
             setattr(ai.model.config, "hidden_dropout", stage["dropout"])
-        if "n_layer" in stage:
-            setattr(ai.model.config, "n_layer", stage["n_layer"])
         inputs = build_inputs(stage)
         ai.train(
             train_data=inputs,

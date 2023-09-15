cd043f01f6282e5b22a5861479e7ca67da06edc5
Luciferian Ink <LuciferianInk@protonmail.com>
2023-09-15 16:06:37 -0500

    o

--- src/lab/book.py
+++ src/lab/book.py
@@ -1,13 +1,24 @@
 import time
+import psutil
 from utils import run_shell_command
 
 
 def main(config):
-    command = run_shell_command(
-        "hugo server --source '/book' --port 42000 --noBuildLock --quiet"
-    )
+    run_shell_command("hugo server --source '/book' --port 42000 --noBuildLock --quiet")
     while True:
         time.sleep(6.66)
+        alive = False
+        for process in psutil.process_iter(["pid", "name"]):
+            try:
+                if process.info["name"] == "hugo":
+                    status = process.status()
+                    if status == "zombie":
+                        continue
+                    alive = True
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                break
+        if not alive:
+            break
 
 
 if __name__ == "main":

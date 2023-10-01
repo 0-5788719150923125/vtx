b7fc8a0a071a91c3da358edc6066b4cadf15f8b2
Luciferian Ink <LuciferianInk@protonmail.com>
2023-09-30 20:10:17 -0500

    o

--- src/head.py
+++ src/head.py
@@ -370,7 +370,7 @@ class cortex:
                     top_k=4,
                     repetition_penalty=2.3,
                     encoder_repetition_penalty=0.999,
-                    exponential_decay_length_penalty=(decay_after_length, decay_factor),
+                    # exponential_decay_length_penalty=(decay_after_length, decay_factor),
                     no_repeat_ngram_size=9,
                     low_memory=self.config.get("low_memory", False),
                     max_time=360,
@@ -378,9 +378,9 @@ class cortex:
                     use_cache=True,
                     renormalize_logits=True,
                     remove_invalid_values=True,
-                    # sequence_bias=push_sequences,
-                    # bad_words_ids=bad_tokens,
-                    # suppress_tokens=suppress_tokens,
+                    sequence_bias=push_sequences,
+                    bad_words_ids=bad_tokens,
+                    suppress_tokens=suppress_tokens,
                     stop_word=propulsion,
                 )
 

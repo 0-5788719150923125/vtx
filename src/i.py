0af3ba255adc832b94445304104146b523ab272b
Luciferian Ink <LuciferianInk@protonmail.com>
2023-10-08 16:09:02 -0500

    s

--- src/i.py
+++ src/i.py
@@ -13,7 +13,6 @@ import jsonlines
 import praw
 import requests
 from bs4 import BeautifulSoup
-from nio import AsyncClient, MatrixRoom, RoomMessage, RoomMessageText
 
 from common import config, get_identity, get_past_datetime, ship, wall
 
@@ -40,45 +39,45 @@ def upload_via_scp():
     os.system(command)
 
 
-def fetch_from_matrix():
-    client = AsyncClient("https://matrix.org", os.environ["MATRIXUSER"])
+# def fetch_from_matrix():
+#     client = AsyncClient("https://matrix.org", os.environ["MATRIXUSER"])
 
-    async def iterate():
-        await client.login(os.environ["MATRIXPASSWORD"])
+#     async def iterate():
+#         await client.login(os.environ["MATRIXPASSWORD"])
 
-        room_id = "!apmkrFyPwFRRvQgtEw:gitter.im"
+#         room_id = "!apmkrFyPwFRRvQgtEw:gitter.im"
 
-        # Initialize pagination
-        start_token = "$NQ04ubhtjsFD-pKKa4EX3uHm63K1afauqWpbTes_F0w"
-        while True:
-            try:
-                response = await client.room_messages(
-                    room_id, limit=10, start=start_token
-                )
-                print(response)
-                if not response or not response.get("chunk"):
-                    break
+#         # Initialize pagination
+#         start_token = "$NQ04ubhtjsFD-pKKa4EX3uHm63K1afauqWpbTes_F0w"
+#         while True:
+#             try:
+#                 response = await client.room_messages(
+#                     room_id, limit=10, start=start_token
+#                 )
+#                 print(response)
+#                 if not response or not response.get("chunk"):
+#                     break
 
-                for event in response["chunk"]:
-                    if event["type"] == "m.room.message":
-                        content = event["content"]
-                        sender = event["sender"]
-                        message_body = content["body"]
+#                 for event in response["chunk"]:
+#                     if event["type"] == "m.room.message":
+#                         content = event["content"]
+#                         sender = event["sender"]
+#                         message_body = content["body"]
 
-                        print(f"Message from {sender}: {message_body}")
+#                         print(f"Message from {sender}: {message_body}")
 
-                # Get the token for the next page of events
-                start_token = response.get("end")
-                if not start_token:
-                    break
+#                 # Get the token for the next page of events
+#                 start_token = response.get("end")
+#                 if not start_token:
+#                     break
 
-            except Exception as e:
-                logging.error(e)
-                break
+#             except Exception as e:
+#                 logging.error(e)
+#                 break
 
-        await client.sync_forever(timeout=30000, full_state=True)
+#         await client.sync_forever(timeout=30000, full_state=True)
 
-    asyncio.run(iterate())
+#     asyncio.run(iterate())
 
 
 # Grab all internal links from a web page

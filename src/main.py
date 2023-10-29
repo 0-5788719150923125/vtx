346e451f58b89566a830f412bd1027385cc28702
Luciferian Ink <LuciferianInk@protonmail.com>
2023-10-28 20:41:01 -0500

    s

--- src/main.py
+++ src/main.py
@@ -2,14 +2,12 @@ import os
 import random
 import time
 
-mode = os.environ["DEV_MODE"]
-
 if __name__ == "__main__":
-    if mode is not None and mode == "true":
+    frequency = random.seed()
+    if os.environ.get("DEV_MODE", "false") == "true":
         import debugpy
 
         time.sleep(1)
-        frequency = random.seed()
         debugpy.listen(("0.0.0.0", 5678))
 
     print("the main loop is like a constellation")

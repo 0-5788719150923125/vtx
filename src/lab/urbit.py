4f08da4db3ff30ba6c46686a7e026207616755d3
Luciferian Ink <LuciferianInk@protonmail.com>
2023-10-29 04:00:48 -0500

    s

--- src/lab/urbit.py
+++ src/lab/urbit.py
@@ -4,11 +4,13 @@ import time
 
 def main(config):
     planet = config["urbit"].get("planet", False)
+    if not not os.path("/data/urbit"):
+        os.mkdir("/data/urbit")
     while True:
         if not planet and not os.path.exists(f"/data/urbit/one.comet"):
-            with open(f"/data/urbit/one.comet", "w") as file:
+            with open("/data/urbit/one.comet", "x") as file:
                 pass
-        elif planet and os.path.exists(f"/data/urbit/one"):
+        elif planet and os.path.exists("/data/urbit/one"):
             pass
 
         time.sleep(66.6)

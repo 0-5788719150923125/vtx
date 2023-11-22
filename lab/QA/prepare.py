40245ef59b7d4248c692605978b2a544e84f4791
Luciferian Ink <LuciferianInk@protonmail.com>
2023-11-22 04:23:56 -0600

    s

--- lab/QA/prepare.py
+++ lab/QA/prepare.py
@@ -1,15 +1,16 @@
+import json
 import os
+import random
 import shutil
 import sys
 import traceback
-
-import jsonlines
 
 sys.path.append("/src")
 
 from common import get_identity, ship, wall
 
 root_dir = "/lab/QA"
+duplicates = 5
 
 
 def main():
@@ -18,19 +19,24 @@ def main():
     if not os.path.exists(f"{root_dir}/train"):
         os.makedirs(f"{root_dir}/train")
 
-    with jsonlines.open(f"{root_dir}/source/data.jsonl") as reader:
+    with open(f"{root_dir}/source/data.jsonl", "r") as file:
+        json_lines = [json.loads(line) for line in file]
         el = 0
-        for obj in reader:
-            try:
-                extracted = list(obj.values())
-                string = f"""{wall}{get_identity()}{ship} {extracted[0]}
+        count = 0
+        while count < duplicates:
+            random.shuffle(json_lines)
+            for obj in json_lines:
+                try:
+                    extracted = list(obj.values())
+                    string = f"""{wall}{get_identity()}{ship} {extracted[0]}
 {wall}{get_identity()}{ship} {extracted[1]}"""
-                with open(f"{root_dir}/train/question{el}.txt", "w") as f:
-                    el = el + 1
+                    with open(f"{root_dir}/train/question{el}.txt", "w") as f:
+                        el = el + 1
 
-                    f.write(string)
-            except Exception as e:
-                print(traceback.format_exc())
+                        f.write(string)
+                except Exception as e:
+                    print(traceback.format_exc())
+            count += 1
 
 
 if __name__ == "__main__":

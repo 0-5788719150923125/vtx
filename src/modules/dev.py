bb020d1527e7b0f9d3550a403188254daba94f44
Luciferian Ink <LuciferianInk@protonmail.com>
2024-08-08 20:53:39 -0500

    o

--- src/modules/dev.py
+++ src/modules/dev.py
@@ -7,6 +7,16 @@ if os.environ.get("DEV_MODE", "false") == "true":
     time.sleep(1)
     debugpy.listen(("0.0.0.0", 5678))
 
+# ----- Mamba ------ #
+# from transformers import MambaConfig, MambaForCausalLM, AutoTokenizer
+# import torch
+
+# tokenizer = AutoTokenizer.from_pretrained("state-spaces/mamba-130m-hf")
+# model = MambaForCausalLM.from_pretrained("state-spaces/mamba-130m-hf")
+# input_ids = tokenizer("Hey how are you doing?", return_tensors="pt")["input_ids"]
+
+# out = model.generate(input_ids, max_new_tokens=10)
+# print(tokenizer.batch_decode(out))
 
 # ----- RWKV v5 ------ #
 # import torch

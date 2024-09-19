a5a6ce9893e036cc9245ecebdc1e158763c2c3f3
Luciferian Ink <LuciferianInk@protonmail.com>
2024-09-19 11:32:40 -0500

    o

--- prepare.py
+++ prepare.py
@@ -1,4 +1,11 @@
 import nltk
 
-nltk.download("punkt")
+try:
+    nltk.download("punkt")
+except Exception as e:
+    try:
+        nltk.download("punkt_tab")
+    except Exception as e:
+        print(e)
+
 nltk.download("stopwords")
